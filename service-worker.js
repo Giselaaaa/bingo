7eaecf9ae3b9bf5631e329c1ab305f4343b18cc8
gigi <92686026+Giselaaaa@users.noreply.github.com>
2026-09-15 03:26:34 +0200

    Implement service worker caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,39 @@
+const CACHE_NAME = "bingo-v1";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icon-bingo.png"
+];
+
 self.addEventListener("install", event => {
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ARCHIVOS))
+            .then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        self.clients.claim()
+        caches.keys().then(nombres => {
+            return Promise.all(
+                nombres
+                    .filter(nombre => nombre !== CACHE_NAME)
+                    .map(nombre => caches.delete(nombre))
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        fetch(event.request)
+        caches.match(event.request)
+            .then(respuesta => {
+                return respuesta || fetch(event.request);
+            })
     );
 });
